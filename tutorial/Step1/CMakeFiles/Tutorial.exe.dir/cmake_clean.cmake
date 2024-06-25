c217fe2891fa8bd07c3efda4fd54040aea88f9ac
korizma <dulekorizma55@gmail.com>
2024-06-25 18:49:01 +0200

    inital commit with the step 1 of the tutorial done

---
+++ tutorial/Step1/CMakeFiles/Tutorial.exe.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tutorial.exe.dir/tutorial.cxx.o"
+  "CMakeFiles/Tutorial.exe.dir/tutorial.cxx.o.d"
+  "Tutorial.exe"
+  "Tutorial.exe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tutorial.exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
